ad6d459e166fd688027b59e24460b57d3054fce2
Gitrom-coder <128481968+Gitrom-coder@users.noreply.github.com>
2024-09-27 23:57:43 +0300

    Add files via upload

---
+++ dicts.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'orange', 'kiwi', 'banana' ]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:3])
+my_list[3]='Lime'
+print(my_list)
+
+my_dict = {'House': 'Дом', 'Cat': 'Кошка', 'Language': 'Язык', 'Dog': 'Собака' }
+print(my_dict)
+print(my_dict['House'])
+my_dict['Cat'] = 'Киса'
+print(my_dict)
+
